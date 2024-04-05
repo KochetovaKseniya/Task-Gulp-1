e6824355620beff83e1afa4e71eb6f67651cf088
KochetovaKseniya <kseniyakochetova01@gmail.com>
2024-04-05 12:41:13 +0300

    task_3

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 
 gulp.task('styles', function() {
   return gulp.src('src/css/*.scss')
@@ -22,3 +24,15 @@ gulp.task('scripts', function() {
 });
 
 gulp.task('default', gulp.parallel('styles', 'scripts'));
+
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+
+gulp.task('js', function() {
+    return gulp.src('src/js/*.js') 
+        .pipe(concat('bundle.js')) 
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js')); 
+});
+
+gulp.task('default', gulp.series('js'));
