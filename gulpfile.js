2d743a4f3d4f9132f0b0717bede24b6915f1802a
KochetovaKseniya <kseniyakochetova01@gmail.com>
2024-04-05 13:29:57 +0300

    update plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,33 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const cleanCSS = require('gulp-clean-css');
-const uglify = require('gulp-uglify');
-const rename = require('gulp-rename');
-const concat = require('gulp-concat');
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const cleanCSS = require("gulp-clean-css");
+const autoprefixer = require("gulp-autoprefixer");
+const rename = require("gulp-rename");
+
 const uglify = require('gulp-uglify');
 
-gulp.task('styles', function() {
-  return gulp.src('src/css/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer())
-    .pipe(cleanCSS())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('dist/css'));
+
+gulp.task("styles", function () {
+    return gulp
+      .src("src-green-2023/scss/**/*.+(scss|sass)")
+      .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+      .pipe(rename({ suffix: ".min", prefix: "" }))
+      .pipe(autoprefixer())
+      .pipe(cleanCSS({ compatibility: "ie8" }))
+      .pipe(gulp.dest("assets-green-2023/min-css"));
 });
 
 gulp.task('scripts', function() {
-  return gulp.src('src/js/*.js')
-    .pipe(uglify())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('dist/js'));
+    return gulp
+      .src(['src-green-2023/js/*.js'])
+      .pipe(rename({ suffix: ".min", prefix: "" }))
+      .pipe(uglify())
+      .pipe(gulp.dest('assets-green-2023/min-js'));
 });
 
-gulp.task('default', gulp.parallel('styles', 'scripts'));
-
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
-
-gulp.task('js', function() {
-    return gulp.src('src/js/*.js') 
-        .pipe(concat('bundle.js')) 
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js')); 
+gulp.task("watch", function () {
+  gulp.watch("src-green-2023/scss/**/*.+(scss|sass|css)", gulp.parallel("styles"));
+  gulp.watch('src-green-2023/js/*.js', gulp.series("scripts"));
 });
 
-gulp.task('default', gulp.series('js'));
+gulp.task("default", gulp.parallel("styles", "scripts", "watch"));
