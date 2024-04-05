692501fcdfca749640bde5a6fc8f3e4f1dc770c0
KochetovaKseniya <kseniyakochetova01@gmail.com>
2024-04-05 12:34:21 +0300

    task2

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ Version 1.0
 To start using enter command in terminal
   npm install --save-dev gulp  
   gulp copy
+
+
+Version 1.1
+To start using enter command in terminal
+  npm install --save-dev gulp  
+  npm install --save-dev gulp-sass gulp-autoprefixer gulp-clean-css gulp-uglify gulp-rename
+
+To compile and compress CSS files, run the `gulp styles` command.
+To compress JavaScript files, run the `gulp scripts` command.
+To perform both tasks at the same time, run the `gulp` command.
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,24 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 
-gulp.task('copy-files', function() {
-  return gulp.src('src/**/*')
-    .pipe(gulp.dest('dist'));
+gulp.task('styles', function() {
+  return gulp.src('src/css/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(cleanCSS())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('dist/css'));
 });
 
-exports.copy = gulp.series('copy-files');
+gulp.task('scripts', function() {
+  return gulp.src('src/js/*.js')
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('default', gulp.parallel('styles', 'scripts'));
